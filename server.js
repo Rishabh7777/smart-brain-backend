282fdd674aaf782d3cd2230d13ebb3dee258aa41
Rishabh7777 <rishabh.tiwari_cs18@gla.ac.in>
2021-11-12 10:25:31 +0530

    fix

--- server.js
+++ server.js
@@ -50,7 +50,6 @@ app.post("/register", (req, res) => {
 		return res.status(400).send("Unable to register");
 	}
 	const hash = bcrypt.hashSync(password, 10);
-	console.log(process.env.DATABASE_URL);
 	// transaction allows to either complete whole or none
 	db.transaction(trx => {
 		// trx is acting as db
