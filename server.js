041707b8b344acf9b9ce1375b8053b60164062a9
Rishabh7777 <rishabh.tiwari_cs18@gla.ac.in>
2021-11-13 11:00:42 +0530

    minor

--- server.js
+++ server.js
@@ -73,7 +73,10 @@ app.post("/register", (req, res) => {
 		.then(trx.commit)
 		.catch(trx.rollback);
 	})
-	.catch(err => res.status(400).json("Unable to store info"))
+	.catch(err => {
+		res.status(400).json("Unable to store info")
+		console.log(err)
+	})
 })
 
 // user profile
